3a172566b80c04f8aa66c552112d46723d9cf3b6
Udhayasuriyan <Udhayasuriyan.m@honeywell.com>
2016-12-10 18:22:42 +0530

    Commit

---
+++ GitHub/src/GitHubTest/GitTest.java
@@ -0,0 +1,12 @@
+package GitHubTest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		//We can create a git project while creating the project in eclipse or if we have created normal java project, we can share the project
+		
+		System.out.println("Hello world");
+
+	}
+
+}
